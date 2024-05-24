dbd97091692f7ab771936afead36a1a89e467934
Alfredodaniello <alfredo_daniello@libero.it>
2024-05-24 19:27:26 +0200

    feat: add foreach for stamp all movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>ciao</div>
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">ECCO I TUOI FILM!</h1>
+            @foreach ($movies as $movie)
+                <div class="col-3">
+                    <div class="card my-3" style="width: 18rem;">
+                        <div class="card-body">
+                          <h5 class="card-title">Titolo: {{$movie->title}}</h5>
+                          <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{$movie->original_title}}</h6>
+                          <div>Nazionalità: {{$movie->nationality}}</div>
+                          <div>Voto: {{$movie->vote}}</div>
+                        </div>
+                      </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
